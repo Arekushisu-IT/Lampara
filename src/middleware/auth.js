6128df58f45f33542383259af099d9d8d03eabf3
Arekushisu . <delapenaalexis315@gmail.com>
2026-03-21 15:23:06 +0800

    Auth updated code

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,18 +1,4 @@
-// File: routes/auth.js
-const express = require('express');
-const router = express.Router();
-
-const verifyToken = require('../src/middleware/auth');
-const { validatePlayerRegister, validate } = require('../src/middleware/validation');
-const { adminLogin, playerLogin, playerLogout, getMe, adminRegister, playerRegister } = require('../src/controllers/authController');
-
-router.post('/login', adminLogin);
-router.post('/register', adminRegister);
-
-router.post('/player-login', playerLogin);
-router.post('/player-logout', playerLogout);
-
-router.post('/player-register', validatePlayerRegister, validate, playerRegister);
+// File: src/middleware/auth.js
 // File: src/middleware/auth.js
 const jwt = require('jsonwebtoken');
 
@@ -33,6 +19,3 @@ const verifyToken = (req, res, next) => {
 };
 
 module.exports = verifyToken;
-router.get('/me', verifyToken, getMe); 
-
-module.exports = router;
