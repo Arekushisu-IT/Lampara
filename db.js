6fcee87143359ca8032fe02c07fd197b1cf4be2c
Arekushisu . <delapenaalexis315@gmail.com>
2026-04-01 14:54:57 +0800

    Sql route Updated

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 // Create connection pool for better performance
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'mysql.railway.internal',
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST || 'shortline.proxy.rlwy.net',
+  port: process.env.DB_PORT || 20695,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'frMKAvEYgdNzhSwsLlUbNWQruLlmMIfc',
+  password: process.env.DB_PASSWORD || 'GWfTGOjllDMkISGtmudCOsoYoscLoiBK',
   database: process.env.DB_NAME || 'lampara_database',
   waitForConnections: true,
   connectionLimit: 10,
