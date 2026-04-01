676c795d8eeba8ec66dae176a83e7bd1bf3fad91
Arekushisu . <delapenaalexis315@gmail.com>
2026-04-01 15:34:52 +0800

    updated

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'shortline.proxy.rlwy.net',
   port: process.env.DB_PORT || 20695,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'GWfTGOjllDMkISGtmudCOsoYoscLoiBK',
+  password: process.env.DB_PASSWORD || 'frMKAvEYgdNzhSwsLlUbNWQruLlmMIfc',
   database: process.env.DB_NAME || 'lampara_database',
   waitForConnections: true,
   connectionLimit: 10,
